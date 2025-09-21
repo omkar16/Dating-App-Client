c18ebd88750714a4fc172e74c562069c4f4fe023
user.email <theomkarbhagwat@gmail.com>
2025-09-21 20:31:08 +0530

    Added route guard so that routes are not accessible if user is not logged in. Also added loading in provideAppInitializer() in app.config.ts to add loading if browser is refreshed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { MemberDetailed } from '../features/members/member-detailed/member-detai
 import { MemberList } from '../features/members/member-list/member-list';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'members', component: MemberList },
-  { path: 'members/:id', component: MemberDetailed },
-  { path: 'lists', component: Lists },
-  { path: 'messages', component: Messages },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MemberList },
+      { path: 'members/:id', component: MemberDetailed },
+      { path: 'lists', component: Lists },
+      { path: 'messages', component: Messages },
+    ],
+  },
+
   { path: '**', component: Home },
 ];
