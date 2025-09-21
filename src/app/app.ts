a8008d15dd7aed70e14eacb82d78a51d10e68529
user.email <theomkarbhagwat@gmail.com>
2025-09-21 20:49:38 +0530

    Cleared unwanted code in app.ts and added route transition

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, signal, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,31 +8,6 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = 'Dating App';
-  protected members = signal<User[]>([]);
+export class App {
   public router = inject(Router);
-
-  async ngOnInit() {
-    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
-      next: (response) => this.members.set(response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request completed'),
-    });
-    this.setCurrentUser();
-    // this.members.set(await this.getMembers());
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  // async getMembers() {
-  //   return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
-  // }
 }
