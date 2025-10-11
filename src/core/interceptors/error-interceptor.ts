d898186dae3a7602883075942c494f0a5e6aae53
user.email <theomkarbhagwat@gmail.com>
2025-10-11 22:22:02 +0530

    Added errors inside application

--- src/core/interceptors/error-interceptor.ts
+++ src/core/interceptors/error-interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { ToastService } from '../services/toast-service';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toast = inject(ToastService);
@@ -13,16 +13,27 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       if (error) {
         switch (error.status) {
           case 400:
-            toast.error(error.error);
+            if (error.error.errors) {
+              const modelStateErrors = [];
+              for (const key in error.error.errors) {
+                if (error.error.errors[key]) {
+                  modelStateErrors.push(error.error.errors[key]);
+                }
+              }
+              throw modelStateErrors.flat();
+            } else {
+              toast.error(error.error + ' ' + error.status);
+            }
             break;
           case 401:
             toast.error('Unauthorized');
             break;
           case 404:
-            toast.error('Not Found');
+            router.navigateByUrl('/not-found');
             break;
           case 500:
-            toast.error('Server Error');
+            const navigationExtras: NavigationExtras = { state: { error: error.error } };
+            router.navigateByUrl('/server-error', navigationExtras);
             break;
 
           default:
---
+++ src/types/error.ts
@@ -0,0 +1,5 @@
+export type ApiError = {
+  messsage: string;
+  statusCode: number;
+  details?: string[];
+};
